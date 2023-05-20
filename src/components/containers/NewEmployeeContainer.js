34a23821f59178c48087c3fad3d1f2d183096737
Ishika Soni <ishikasoni50@gmail.com>
2023-05-20 14:40:17 -0400

    new files added

---
+++ src/components/containers/NewEmployeeContainer.js
@@ -0,0 +1,81 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import NewEmployeeView from "../views/NewEmployeeView";
+import { addEmployeeThunk } from "../../store/thunks";
+
+class NewEmployeeContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      department: "",
+      id: "",
+      redirect: false,
+      redirectId: null,
+      error: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    if (this.state.firstname === "") {
+      this.setState({ error: "Name field is required" });
+      return;
+    }
+    if (this.state.lastname === "") {
+      this.setState({ error: "Name field is required" });
+      return;
+    }
+    if (this.state.id === "") {
+        this.setState({ error: "Id field is required" });
+        return;
+      }
+    let employee = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      department: this.state.department,
+      id: this.state.id
+    };
+
+    let newEmployee = await this.props.addEmployee(employee);
+
+    this.setState({
+      redirect: true,
+      redirectId: newEmployee.id,
+      error: "",
+    });
+  };
+
+  componentWillUnmount() {
+    this.setState({ redirect: false, redirectId: null });
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={`/employee/${this.state.redirectId}`} />;
+    }
+    return (
+      <NewEmployeeView
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        error={this.state.error}
+      />
+    );
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    addEmployee: (employee) => dispatch(addEmployeeThunk(employee)),
+  };
+};
+
+export default connect(null, mapDispatch)(NewEmployeeContainer);
--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -6,5 +6,7 @@ export { default as AllTasksContainer } from "./AllTasksContainer";
 export { default as TaskContainer } from "./TaskContainer";
 export { default as NewTaskContainer } from "./NewTaskContainer";
 export { default as EditTaskContainer } from "./EditTaskContainer";
+export { default as NewEmployeeContainer } from "./NewEmployeeContainer";
+
 
 
